0d5ba219978d14e6906670188c23b4e820bb881b
anilsaini81155@gmail.com <anilsaini81155@gmail.com>
2022-02-24 09:39:03 +0530

    Changes done

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'mastermysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -41,6 +41,23 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
+
+        'mastermysql' => [
+            'sticky' => true,
+            'driver' => 'mysql',
+            'host' => env('DB_HOST_WRITE', '127.0.0.1'),
+            'port' => env('DB_PORT_WRITE', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => false,
+            'engine' => null,
         ],
 
         'mysql' => [
